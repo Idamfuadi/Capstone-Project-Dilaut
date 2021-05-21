f03102dc96ec24ab5d8bdac0f4c9988cf966221e
Mohamad Idam Fuadi <mohamadidam123@gmail.com>
2021-05-21 08:58:18 +0700

    menambahkan UI main activity

---
+++ app/src/main/java/com/dicoding/idam/dilaut/DetailAreaActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.idam.dilaut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAreaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_area)
+    }
+}
