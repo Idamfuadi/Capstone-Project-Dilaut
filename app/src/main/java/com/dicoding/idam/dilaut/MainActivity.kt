13ec30724d65d85511038ddb41d36fae887dbd0e
latiefniam <latiefniam@gmail.com>
2021-05-25 14:39:34 +0700

    menghapus percobaan

--- app/src/main/java/com/dicoding/idam/dilaut/MainActivity.kt
+++ app/src/main/java/com/dicoding/idam/dilaut/MainActivity.kt
@@ -9,12 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun haiiii(){
-
-    }
-    fun bermain(){
-        //cek sync github
-    }
-
 
 }
