3de3acc2922d1906719c0ed4bd238facf63abf46
latiefniam <latiefniam@gmail.com>
2021-05-25 14:59:38 +0700

    SplashScreen

---
+++ app/src/main/java/com/dicoding/idam/SplashScreen/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.dicoding.idam.SplashScreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.idam.dilaut.MainActivity
+import com.dicoding.idam.dilaut.R
+
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var handler : Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        handler= Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },1000)
+    }
+}
