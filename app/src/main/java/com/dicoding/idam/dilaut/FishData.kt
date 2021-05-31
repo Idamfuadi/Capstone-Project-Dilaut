48ccba6efd9f603990e0c558c21de9905c87b35e
Mohamad Idam Fuadi <mohamadidam123@gmail.com>
2021-05-31 21:41:24 +0700

    edit splash screen and temporary data offline

---
+++ app/src/main/java/com/dicoding/idam/dilaut/FishData.kt
@@ -0,0 +1,39 @@
+package com.dicoding.idam.dilaut
+
+object FishData {
+    private val fishName = arrayOf(
+            "Gurami",
+            "Kakap Putih",
+            "Kerapu",
+            "Lele",
+            "Nila",
+            "Patin",
+            "Mujair",
+            "Ikan Mas",
+            "Tawes",
+            "Udang Vaname")
+
+    private val fishDetail = arrayOf(
+            "Ini deskripsi untuk Gurami",
+            "Ini deskripsi untuk Kakap Putih",
+            "Ini deskripsi untuk Kerapu",
+            "Ini deskripsi untuk Lele",
+            "Ini deskripsi untuk Nila",
+            "Ini deskripsi untuk Patin",
+            "Ini deskripsi untuk Mujair",
+            "Ini deskripsi untuk Ikan Mas",
+            "Ini deskripsi untuk Tawes",
+            "Ini deskripsi untuk Udang Vaname")
+
+    private val fishImage = arrayOf(
+            R.drawable.gurami,
+            R.drawable.kakap_putih,
+            R.drawable.ikan_kerapu,
+            R.drawable.lele,
+            R.drawable.nila,
+            R.drawable.patin,
+            R.drawable.mujair,
+            R.drawable.ikan_mas,
+            R.drawable.tawes,
+            R.drawable.udang_vaname)
+}
